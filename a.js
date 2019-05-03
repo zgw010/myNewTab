249aa03e41f277dc7d705d6bf736cc1a9bbb7b82
ZGW <lleaff@qq.com>
2019-05-03 17:19:47 +0800

    layout & style uodate

--- a.js
+++ a.js
@@ -4,4 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const nowDate = new Date();
     time.innerText = `${nowDate.getHours()}:${nowDate.getMinutes()}:${nowDate.getSeconds()}`
   }, 0, 1000);
+  // document.querySelector("html").requestFullscreen();
+  document.addEventListener('click', function () {
+    document.querySelector("html").requestFullscreen();
+    document.querySelector("body").style.background = "url('https://source.unsplash.com/random/1920x1080')"
+    document.querySelector(".time").style.display = "inline"
+    console.log(document.querySelector("body"));
+    let style=document.createElement('style');
+    style.innerHTML="body::after{top:-10000px}";//添加样式内容的话也可以用上面提到过的`insertRule`,相对例子里的硬编码会更优雅点。
+    document.head.appendChild(style);
+  });
 });
